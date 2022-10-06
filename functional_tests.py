419a258d28b5a6afba1aa10bf55f7c6a9d4eb98b
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2022-10-06 09:57:52 -0300

    🎁Atualiza caso de teste com uso de unittest e classes
    Texto do caso de teste atualizado de forma a seguir método baseado em classes.

--- functional_tests.py
+++ functional_tests.py
@@ -1,37 +1,64 @@
 """
 Testes funcionais do Django.
 """
+import unittest
+
 from selenium import webdriver
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-# Edith ouviu falar de uma nova aplicação online interessante para lista de tarefas. Ela decide
-# verificar sua homepage.
+class NewVisitorTest(unittest.TestCase):
+    """Teste de novo visitante."""
+    def setUp(self):
+        """
+        Configurações iniciais do caso de teste
+        :return:
+        """
+        # * Edith tem um novo navegador
+        self.browser = webdriver.Firefox()
 
-# * Mensagem padrão do Django 4.0 para testar se o servidor está funcionando.
-assert 'The install worked successfully! Congratulations!' in browser.title
+    def tearDown(self):
+        """
+        Finalização do caso de teste
+        :return:
+        """
+        # * Edith fecha o navegador
+        self.browser.quit()
 
-# Ela percebe que o título da página e o cabeçalho mencionam listas de tarefas (to-do)
-assert 'To-Do' in browser.title
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        Teste: Edith pode iniciar uma lista e recuperá-la mais tarde
+        :return:
+        """
+        # Edith ouviu falar de uma nova aplicação online interessante para lista de tarefas. Ela decide
+        # verificar sua homepage.
+        self.browser.get('http://localhost:8000')
 
-# Ela é convidada a inserir um item de tarefa imediatamente.
+        # Ela percebe que o título da página e o cabeçalho mencionam listas de tarefas (to-do)
+        self.assertIn('To-Do', self.browser.title)
+        # * Aqui o teste simplesmente falha. Utilizado para lembrar sobre a finalização do teste.
+        self.fail('Termine o teste!')
 
-# Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa de texto (o hobby
-# de Edith é fazer iscas # para pesca com fly).
+        # Ela é convidada a inserir um item de tarefa imediatamente.
 
-# Quando ela tecla enter, a página é atualizada, e agora a página lista
-# "1: Buy peacock feathers" como um item em uma lista de tarefas.
+        # Ela digita "Buy peacock feathers" (Comprar penas de pavão) em uma caixa de texto (o hobby
+        # de Edith é fazer iscas # para pesca com fly).
 
-# Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
-# Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão para fazer
-# um fly - Edith é bem metódica).
+        # Quando ela tecla enter, a página é atualizada, e agora a página lista
+        # "1: Buy peacock feathers" como um item em uma lista de tarefas.
 
-# A página é atualizada novamente e agora mostra os dois itens em sua lista.
+        # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
+        # Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão para fazer
+        # um fly - Edith é bem metódica).
 
-# Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site gerou um URL
-# único para ela - há um pequeno texto explicativo para isso.
+        # A página é atualizada novamente e agora mostra os dois itens em sua lista.
 
-# Ela acessa esse URL - sua lista de tarefas continua lá.
+        # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site gerou um URL
+        # único para ela - há um pequeno texto explicativo para isso.
 
-# Satisfeita, ela volta a dormir.
+        # Ela acessa esse URL - sua lista de tarefas continua lá.
+
+        # Satisfeita, ela volta a dormir.
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
