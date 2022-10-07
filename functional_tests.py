91e7893b67efc7a345ba46e95e3b3c6bc82aa30d
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2022-10-07 10:13:35 -0300

    🎨Duplicação retirada
    Verifica-se que o erro sobre o teste funcional continua o mesmo

--- functional_tests.py
+++ functional_tests.py
@@ -3,14 +3,15 @@ Testes funcionais do Django.
 """
 import time
 import unittest
-import test
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
+
 class NewVisitorTest(unittest.TestCase):
     """Teste de novo visitante."""
+
     def setUp(self):
         """
         Configurações iniciais do caso de teste
@@ -27,6 +28,16 @@ class NewVisitorTest(unittest.TestCase):
         # * Edith fecha o navegador
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        """
+        Verifica se uma linha está na tabela de lista
+        :param row_text:
+        :return:
+        """
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
         Teste: Edith pode iniciar uma lista e recuperá-la mais tarde
@@ -38,7 +49,7 @@ class NewVisitorTest(unittest.TestCase):
 
         # Ela percebe que o título da página e o cabeçalho mencionam listas de tarefas (to-do)
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element(By.TAG_NAME,'h1').text
+        header_text = self.browser.find_element(By.TAG_NAME, 'h1').text
         self.assertIn('To-Do', header_text)
 
         # Ela é convidada a inserir um item de tarefa imediatamente.
@@ -55,11 +66,9 @@ class NewVisitorTest(unittest.TestCase):
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas.
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(5)
+        time.sleep(1)
 
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
         # Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão para fazer
@@ -70,10 +79,8 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lista.
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn('2: Use peacock feathers to make a fly', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site gerou um URL
         # único para ela - há um pequeno texto explicativo para isso.
