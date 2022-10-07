e797d0edb4586956950360a8755be6027077b99e
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2022-10-07 08:29:13 -0300

    🎨Refatorada a view da home page para usar um template

--- lists/tests.py
+++ lists/tests.py
@@ -1,23 +1,11 @@
 """
 Testes de unidade do aplicativo lists.
 """
-from django.http import HttpRequest
 from django.test import TestCase
-from django.urls import resolve
-
-from lists.views import home_page
 
 
 class HomePageTest(TestCase):
     """Teste de página inicial."""
-
-    def test_root_url_resolves_to_home_page_view(self):
-        """
-        Teste: URL raiz resolve para a página inicial
-        :return:
-        """
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
         """
@@ -25,10 +13,4 @@ class HomePageTest(TestCase):
         :return:
         """
         response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
-
         self.assertTemplateUsed(response, 'home.html')
