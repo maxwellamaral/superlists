696d85ad449bb523cde9f94e7a6e629f3a854450
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2022-10-07 11:31:41 -0300

    🎨Reduzido o sleep vodu

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -5,8 +5,11 @@ import time
 
 from django.test import LiveServerTestCase
 from selenium import webdriver
+from selenium.common import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -38,6 +41,24 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements(By.TAG_NAME, 'tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    def wait_for_row_in_list_table(self, row_text):
+        """
+        Espera pela lista de dados antes de realizar o teste
+        :param row_text:
+        :return:
+        """
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element(By.ID, 'id_list_table')
+                rows = table.find_elements(By.TAG_NAME, 'tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         """
         Teste: Edith pode iniciar uma lista e recuperá-la mais tarde
@@ -66,9 +87,7 @@ class NewVisitorTest(LiveServerTestCase):
         # Quando ela tecla enter, a página é atualizada, e agora a página lista
         # "1: Buy peacock feathers" como um item em uma lista de tarefas.
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar outro item.
         # Ela insere "Use peacock feathers to make a fly" (Usar penas de pavão para fazer
@@ -76,11 +95,10 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element(By.ID, 'id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # A página é atualizada novamente e agora mostra os dois itens em sua lista.
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # Edith se pergunta se o site lembrará de sua lista. Então ela nota que o site gerou um URL
         # único para ela - há um pequeno texto explicativo para isso.
