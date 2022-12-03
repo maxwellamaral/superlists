e8996696f4fcf665571caf19edb6cde9b6d57415
Maxwell Anderson <maxwell.amaral@ifpb.edu.br>
2022-12-03 16:37:02 -0300

    🎨Novo form para items de lista

---
+++ lists/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+
+from lists.models import Item
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+    """
+    Formulário de item.
+    """
+
+    class Meta:
+        model = Item
+        fields = ('text',)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                'placeholder': 'Enter a to-do item',
+                'class': 'form-control input-lg',
+            }),
+        }
+        # ! Para personalizar o erro de formulário, você pode usar o atributo error_messages
+        # ! Neste caso, quero personalizar o erro de validação de campo vazio ou campo requerido.
+        # ! Quando um erro de formulário ocorre, o Django preenche uma lista de erros de formulário em forms.errors.
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
